2a03c24463fda5bdcb217960b4b22cbe5edde5ef
Rabindranath Ferreira <ferreiralox@gmail.com>
2019-12-06 00:18:17 -0300

    timestamp endpoint

--- server.js
+++ server.js
@@ -13,10 +13,60 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+var monthNames = [ "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December" ];
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+
+app.all('/:timestamp', function (request, response) {
+
+    var timeStamp = request.params.timestamp;
+    
+    var timeObj = {
+        "unix" : null,
+        "natural": null
+    };
+    
+    try {
+        
+        var naturalTime = timeStamp.toString().split(' ');
+        
+        if (naturalTime.length === 3) {
+            
+            var dt = new Date(timeStamp.toString());
+            timeObj["natural"] = timeStamp.toString(); 
+            timeObj["unix"] = dt.valueOf() / 1000;
+            //response.write('Natural Date!');
+
+        } else {
+            throw new Error('Not Natural Date!'); 
+        }
+
+    } catch (err1) {
+        console.log(err1);
+        try {
+            
+            var unixTime = Number(timeStamp);
+            
+            if (isNaN(unixTime))  { 
+                throw new Error('Not Unix Timestamp!');
+            } else {
+                timeObj["unix"] = unixTime;
+                
+                var dt = new Date(unixTime * 1000);
+                timeObj["natural"] = monthNames[dt.getMonth()] + " " + dt.getDate() + ", " + dt.getFullYear();
+            } 
+        } catch (err2) {
+            console.log(err2);
+        }
+    }
+
+    response.send(timeObj);
+});
+
 
 
 // your first API endpoint... 
